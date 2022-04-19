7e659e39889ba42e7cd8ca54f7c1caea9cab6f56
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-04-19 16:00:17 -0500

    create example 2-7

---
+++ weekly_mission_2/examples/examples2/examples2-7.js
@@ -0,0 +1,37 @@
+console.log("Ejemplo 7: Setters")
+class MissionCommander {
+    constructor(name, mission){
+        this.name = name
+        this.mission = mission
+        this.students = 0
+        this.lives = 0
+    }
+  
+    get getStudents(){
+     return this.students
+    }
+  
+    get getLives(){
+     return this.lives
+    }
+  
+    set setStudents(students){
+      this.students = students
+    }
+  
+    set setLives(lives){
+      this.lives = lives
+    }
+  }
+  
+
+  const missionCommanderNode = new MissionCommander("Carlo", "NodeJS")
+  
+  console.log(missionCommanderNode.getStudents) 
+  console.log(missionCommanderNode.getLives)
+  
+  missionCommanderNode.setStudents = 100
+  missionCommanderNode.setLives = 3
+  
+  console.log(missionCommanderNode.getStudents) 
+  console.log(missionCommanderNode.getLives)
