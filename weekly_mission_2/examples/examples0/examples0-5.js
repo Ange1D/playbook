dc39bbfc07b90b8e93f08e65df8fc3165d74361c
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-04-18 19:43:13 -0500

    create example 0-5

---
+++ weekly_mission_2/examples/examples0/examples0-5.js
@@ -0,0 +1,13 @@
+console.log("Ejemplo 5: Crear un objeto que reciba parámetros")
+
+
+const ajolonauta2 ={
+    name: "",
+    mission:"Node JS",
+    sayHelloToExplorers: function(yourName){
+        console.log(`Ajolonauta: Saludos ${yourName}`)
+    }
+}  
+
+console.log("Ajolonauta")
+ajolonauta.sayHelloToExplorers("Angel")
