253bf85d97647456d8f41fe7dd5861a80453a2e4
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-04-18 19:28:04 -0500

    create example 0-3

---
+++ weekly_mission_2/examples/examples0/examples0-3.js
@@ -0,0 +1,15 @@
+console.log("Ejemplo 3: Crear un objeto con propiedades diferentes")
+
+const myObjetc3 = {
+    name: "Angel",
+    mision: "Node JS",
+    stack: ["js","node","vue"],
+    blog:{
+        url:"https://ange1d.github.io/my_launchx_blog/"
+    }// esto es un objeto
+}
+
+console.log(myObjetc3)
+console.log("Accediendo a las propiedades del objeto")
+console.log(myObjetc3.name)
+console.log(myObjetc3["mision"])
