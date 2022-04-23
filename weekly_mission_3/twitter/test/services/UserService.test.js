966c8dee76d6a3a8afc516173dfb91176d9952ba
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-04-23 17:56:34 -0500

    Update username test

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -19,4 +19,10 @@ describe("Tests for UserService", () => {
     expect(userInfoInList[3]).toBe("Sin bio")
   })
 
+  test("3. Update username", () => {
+    const user = UserService.create(1, "carlogilmar", "Carlo")
+    UserService.updateUserUsername(user, "carlog")
+    expect(user.username).toBe("carlog")
+  })
+
 })
